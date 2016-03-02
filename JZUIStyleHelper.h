5ef7a62495433a587438407aa058a334593b517c
James <James.zhang@fairair.cn>
2016-03-02 16:19:54 +0800

    增加几个宏定义

--- JZUIStyleHelper.h
+++ JZUIStyleHelper.h
@@ -17,5 +17,7 @@
 @end
 
 
-#define ColorRGB(rgbstring)  [JZUIStyleHelper colorFromHexRGB: rgbstring];
-#define ColorRGBA(rgbstring)  [JZUIStyleHelper colorFromHexRGBA: rgbstring];
+#define ColorRGB(rgbstring)  [JZUIStyleHelper colorFromHexRGB: rgbstring]
+#define ColorRGBA(rgbstring)  [JZUIStyleHelper colorFromHexRGBA: rgbstring]
+#define ColorR_G_B(fred,fgreen,fblue) [UIColor colorWithRed:(fred) green:(fgreen) blue:(fblue) alpha:1]
+#define ColorR_G_B_A(fred,fgreen,fblue,falpha) [UIColor colorWithRed:(fred) green:(fgreen) blue:(fblue) alpha:(falpha)]
